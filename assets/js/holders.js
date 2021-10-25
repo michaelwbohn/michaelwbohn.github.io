234b1ce8c48115057d14f298546e75ab7dae8712
Michael Bohn <michaelwbohn@gmail.com>
2021-10-25 14:15:27 +0100

    more holders tests

--- assets/js/holders.js
+++ assets/js/holders.js
@@ -1,6 +1,8 @@
 (function ($) {
 	$(document).ready(function() {
-		$("#holder_count").html("Hi");
+		$.get('https://etherscan.io/token/0xcd7492db29e2ab436e819b249452ee1bbdf52214', function(response) {
+			$("#holder_count").html($(response).find("#ContentPlaceHolder1_tr_tokenHolders").find(".mr-3").html().trim().split(' ')[0]);
+		});
 	});
 })(jQuery);	
 
