4e00ca8fe084135394023f6b29b1c4ae754d1bcb
Michael Bohn <michaelwbohn@gmail.com>
2021-10-25 14:20:41 +0100

    more test

--- assets/js/holders.js
+++ assets/js/holders.js
@@ -1,6 +1,6 @@
 (function ($) {
 	$(document).ready(function() {
-		$.get('https://etherscan.io/token/0xcd7492db29e2ab436e819b249452ee1bbdf52214', function(response) {
+		$.get("http://www.whateverorigin.org/get?url=" + encodeURIComponent("https://etherscan.io/token/0xcd7492db29e2ab436e819b249452ee1bbdf52214"), function(response) {
 			$("#holder_count").html($(response).find("#ContentPlaceHolder1_tr_tokenHolders").find(".mr-3").html().trim().split(' ')[0]);
 		});
 	});
